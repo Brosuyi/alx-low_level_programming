d8b608e993068ea83d99c4ccb4396e226e0f1146
Brosuyi <jegede.busuyi@gmail.com>
2022-04-13 17:52:01 -0700

    correct the tasks

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/*
+ * main - Prints out the sizes of various data types.
+ * this program is compiled on.
+ * Return: Always 0 (Success)
+ */
 int main (void)
 {
 	printf ("Size of a char: %d byte(s)\n", sizeof(char));
